476076ecbf5900dfa4066f5d152d269da995b697
Alvin Mahendra <yusuf.mahendra@formulatrix.com>
2025-11-04 08:36:40 +0700

    chore: modify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,45 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      import: importPlugin,
+      jsxA11y,
+      prettier,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      react.configs.recommended,
+      jsxA11y.configs.recommended,
+      'plugin:prettier/recommended',
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <div className="w-fit h-fit bg-red-400">React Typescript</div>
+    <div className="w-fit h-fit bg-red-400">
+      React Typescript
+    </div>
   )
 }
 
